453cd41d18d9ab4b62d883cfb0f1aa8d4cd1910a
Bian-1cheng <1194832004@qq.com>
2024-03-12 21:28:56 +0800

    init-0312

---
+++ src/main/java/com/bian/nwucommunication/NwuCommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.bian.nwucommunication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NwuCommunicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NwuCommunicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bian/nwucommunication/NwuCommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bian.nwucommunication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NwuCommunicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
