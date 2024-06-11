6bc8a9e4044acda2e0216d4bc7c6aae09158a582
Bian-1cheng <1194832004@qq.com>
2024-06-11 11:25:51 +0800

    feat: 修改跨域请求类型配置，放行options类型

--- src/main/java/com/bian/nwucommunication/config/CrosConfig.java
+++ src/main/java/com/bian/nwucommunication/config/CrosConfig.java
@@ -1,9 +1,12 @@
 package com.bian.nwucommunication.config;
 
+import com.bian.nwucommunication.util.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -17,7 +20,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CrosConfig implements WebMvcConfigurer {
 
-    static final String ORIGINS[] = new String[]{"GET", "POST", "PUT", "DELETE","OPTIONS"};
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    static final String ORIGINS[] = new String[]{"GET", "POST", "PUT", "DELETE"};
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -27,6 +33,19 @@ public class CrosConfig implements WebMvcConfigurer {
                 .allowedMethods(ORIGINS) // 当前站点支持的跨域请求类型是什么
                 .maxAge(3600); // 超时时长设置为1小时。 时间单位是秒。
     }
+
+
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //假设拦截test接口 后续实际遇到拦截的接口是时，再配置真正的拦截接口
+//        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/fs/login").excludePathPatterns("/register");    //拦截所有，排除登录注册接口
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/fs/getMessage");
+    }
+
+
 }
 
 
