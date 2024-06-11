2c432e4302a0a6a4a5f05d1a1be6bea09dae2a10
Bian-1cheng <1194832004@qq.com>
2024-06-11 15:56:24 +0800

    feat: 获取当前用户的消息

--- src/main/java/com/bian/nwucommunication/dao/Notice.java
+++ src/main/java/com/bian/nwucommunication/dao/Notice.java
@@ -3,6 +3,7 @@ package com.bian.nwucommunication.dao;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -21,6 +22,7 @@ import java.time.LocalDate;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
+@TableName("fsahre_notice")
 public class Notice implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -41,7 +43,7 @@ public class Notice implements Serializable {
     private Integer fileidId;
 
     @TableField("userId_id")
-    private Integer useridId;
+    private Integer userId;
 
     private String keyWord;
 
