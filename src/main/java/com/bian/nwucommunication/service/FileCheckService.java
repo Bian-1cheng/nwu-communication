1bdb74f4b223ddef3e3c3304093b50472e3a4112
Bian-1cheng <1194832004@qq.com>
2024-07-26 20:21:41 +0800

    feat: 视频鉴黄与图片鉴黄

--- src/main/java/com/bian/nwucommunication/service/FileCheckService.java
+++ src/main/java/com/bian/nwucommunication/service/FileCheckService.java
@@ -1,13 +1,12 @@
 package com.bian.nwucommunication.service;
 
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.bian.nwucommunication.dao.FileInfo;
-import com.bian.nwucommunication.dto.FileInfoDTO;
-import com.bian.nwucommunication.dto.req.CheckFileReqDTO;
 
-import java.util.List;
+import java.io.File;
 
 public interface FileCheckService {
 
-    Boolean checkNFSW(String fileUrl);
+    Double checkImgNsfw(String fileUrl,int retryTimes);
+
+    Boolean checkVideoNsfw(File file,int retryTimes);
+
 }
