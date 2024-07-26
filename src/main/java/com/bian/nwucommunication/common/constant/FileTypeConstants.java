87654edb044c5254b18ad05bcc59a9518e29fc32
Bian-1cheng <1194832004@qq.com>
2024-07-26 20:22:49 +0800

    feat: 视频帧的存放路径常量

---
+++ src/main/java/com/bian/nwucommunication/common/constant/FileTypeConstants.java
@@ -0,0 +1,8 @@
+package com.bian.nwucommunication.common.constant;
+
+public class FileTypeConstants {
+
+    // 视频帧的存放路径
+    public static final String VIDEO_IMG_FILE_PATH = System.getProperty("user.dir") + "\\src\\main\\resources\\video\\pic\\";
+    public static final String VIDEO_FILE_PATH = System.getProperty("user.dir") + "/src/main/resources/video/init/";
+}
