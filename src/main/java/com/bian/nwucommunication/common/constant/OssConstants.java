ef1d1dd824aa78d77cb6ea5f84cefd3edd259cf6
Bian-1cheng <1194832004@qq.com>
2024-07-31 20:03:34 +0800

    feat: 对word、pdf文件的处理

--- src/main/java/com/bian/nwucommunication/common/constant/OssConstants.java
+++ src/main/java/com/bian/nwucommunication/common/constant/OssConstants.java
@@ -7,7 +7,8 @@ public class OssConstants {
     public static final String FILE_ADDRESS_PRE = "https://";
 
     // 允许的文件格式
-    public static final String[] ALLOW_TYPE = {"jpg", "jpeg", "png","pdf","mp3","mp4","word","pdf","ppt"};
+    public static final String[] ALLOW_TYPE = {"jpg", "jpeg", "png","pdf","mp3","mp4","docx","doc","ppt"};
     public static final String[] ALLOW_IMG_TYPE = {"jpg", "jpeg", "png"};
     public static final String[] ALLOW_VIDEO_TYPE = {"mp4","mp3"};
+    public static final String[] ALLOW_TEXT_TYPE = {"pdf","doc","docx"};
 }
--- src/main/java/com/bian/nwucommunication/service/impl/FileInfoServiceImpl.java
+++ src/main/java/com/bian/nwucommunication/service/impl/FileInfoServiceImpl.java
@@ -18,15 +18,15 @@ import com.bian.nwucommunication.dao.FileInfo;
 import com.bian.nwucommunication.dto.FileInfoDTO;
 import com.bian.nwucommunication.dto.FileUploadDTO;
 import com.bian.nwucommunication.dto.UserDTO;
+import com.bian.nwucommunication.dto.req.CommentReqDTO;
 import com.bian.nwucommunication.dto.req.RequirementReqDTO;
 import com.bian.nwucommunication.mapper.FileInfoMapper;
-import com.bian.nwucommunication.service.FileCheckService;
-import com.bian.nwucommunication.service.FileInfoService;
-import com.bian.nwucommunication.service.RequirementService;
+import com.bian.nwucommunication.service.*;
 import com.bian.nwucommunication.util.*;
 import com.bian.nwucommunication.util.redis.RedisUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -39,6 +39,8 @@ import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 
+
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -49,7 +51,12 @@ public class FileInfoServiceImpl extends ServiceImpl<FileInfoMapper,FileInfo> im
     private final FileUtil fileUtil;
     private final RequirementService requirementService;
     private final FileCheckService fileCheckService;
+    private final CommentService commentService;
     private final EmailService emailService;
+    private final WenXinAi wenXinAi;
+    private final UserService userService;
+
+    public static final String AI_BOT = "AI机器人";
 
     @Override
     public List<FileInfoDTO> queryMyFile() {
@@ -96,63 +103,62 @@ public class FileInfoServiceImpl extends ServiceImpl<FileInfoMapper,FileInfo> im
         String sensitiveWord = SensitiveWordUtil.checkSensitiveWord(fileUploadDTO);
         if(!StrUtil.isEmpty(sensitiveWord))
             throw new ServiceException(sensitiveWord,BaseErrorCode.SENSITIVE_WORD_EXIST);
-
-        // 将文件暂存到本地
         String fileName = file.getOriginalFilename();
-        File tempFile = new File(FileTypeConstants.VIDEO_FILE_PATH, fileName);
-        tempFile.getParentFile().mkdirs();
-        try (FileOutputStream fos = new FileOutputStream(tempFile);
-            InputStream is = file.getInputStream()) {
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = is.read(buffer)) != -1) {
-                fos.write(buffer, 0, bytesRead);
-            }
-        } catch (IOException e) {
-            log.error("保存文件失败");
-        }
+        File tempFile = saveToLocal(file, fileName);
 
         String ext = fileName.substring(fileName.lastIndexOf(".") + 1);
         double score = 0.0; // 为违规图片的得分，> 0.8 为违规图片
         if(Arrays.asList(OssConstants.ALLOW_IMG_TYPE).contains(ext)){
-            String OssfilePath = null;
-            try {
-                OssfilePath = fileUtil.upload(new FileInputStream(tempFile),file.getOriginalFilename(), OssConstants.FILE_ADDRESS);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            score = fileCheckService.checkImgNsfw(OssfilePath,NsfwConstants.INTERFACE_RETRY_TIMES);
+            String filePath = uploadFile(file, tempFile);
+            score = fileCheckService.checkImgNsfw(filePath,NsfwConstants.INTERFACE_RETRY_TIMES);
         }
         if (score > NsfwConstants.YELLOW_IMG_SCORE)
             throw new ClientException("上传的图片为违规图片");
 
-        CompletableFuture.runAsync(() ->{
-            if(Arrays.asList(OssConstants.ALLOW_VIDEO_TYPE).contains(ext)){
+        if (Arrays.asList(OssConstants.ALLOW_TEXT_TYPE).contains(ext)){
+            CompletableFuture.runAsync(() ->{
+                TextExtractUtil textExtractUtil = new TextExtractUtil();
+                String content = textExtractUtil.fileExtractText(tempFile.getPath());
+                String filePath = uploadFile(file, tempFile);
+                FileInfo fileInfo = convertToFileInfo(fileUploadDTO, user, filePath);
+                int id = fileInfoMapper.insert(fileInfo);
+
+                UserDTO AI = userService.getUserByName(AI_BOT);
+                if(AI == null)
+                    throw new ServiceException(BaseErrorCode.SERVICE_BOT_ERROR);
+                String summary = wenXinAi.getSummary(content);
+                if(!StrUtil.isNullOrUndefined(summary))
+                    commentService.putCommentBaseAI(new CommentReqDTO(fileInfo.getId(),summary),AI);
+            });
+        }
+
+        if(Arrays.asList(OssConstants.ALLOW_VIDEO_TYPE).contains(ext)){
+            CompletableFuture.runAsync(() ->{
                 Boolean IsPass = fileCheckService.checkVideoNsfw(tempFile,NsfwConstants.INTERFACE_RETRY_TIMES);
                 if(!IsPass){
                     emailService.sendWarning(user.getEmail(), fileUploadDTO.getKeyWord());
                     log.error("内容含有非法内容，请修改后重新上传");
                 }
-            }
-            String filePath;
-            try {
-                filePath = fileUtil.upload(new FileInputStream(tempFile),file.getOriginalFilename(), OssConstants.FILE_ADDRESS);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                FileInfo fileInfo = BeanUtil.toBeanIgnoreCase(fileUploadDTO, FileInfo.class, true);
-                fileInfo.setPushDate(LocalDateTimeUtil.parseDate(DateUtil.today()));
-                fileInfo.setUserId(user.getId());
-                fileInfo.setPath(filePath);
-                fileInfo.setIsPass(UserConstants.FILE_WAIT_CHECK);
-                fileInfo.setSchoolId(user.getSchoolId());
-                fileInfo.setSchoolName(SchoolEnum.getNameByCode(user.getSchoolId()));
-                fileInfoMapper.insert(fileInfo);
-            } catch (Exception e) {
-                log.error("保存失败{}",e.getMessage());
-            }
-        });
+                String filePath = uploadFile(file, tempFile);
+                try {
+                    FileInfo fileInfo = convertToFileInfo(fileUploadDTO, user, filePath);
+                    fileInfoMapper.insert(fileInfo);
+                } catch (Exception e) {
+                    log.error("保存失败{}",e.getMessage());
+                }
+            });
+        }
+
+    }
+
+    private String uploadFile(MultipartFile file, File tempFile) {
+        String filePath;
+        try {
+            filePath = fileUtil.upload(new FileInputStream(tempFile), file.getOriginalFilename(), OssConstants.FILE_ADDRESS);
+        } catch (IOException e) {
+            throw new ServiceException(BaseErrorCode.REMOTE_ERROR);
+        }
+        return filePath;
     }
 
     @Override
@@ -182,5 +188,34 @@ public class FileInfoServiceImpl extends ServiceImpl<FileInfoMapper,FileInfo> im
             redisTemplate.opsForValue().set(RedisConstants.CACHE_All_School_KEY+item.getId()+":",JSONUtil.toJsonStr(item));
         }
         return fileInfoDTOS;
+    }
+
+    @NotNull
+    private static FileInfo convertToFileInfo(FileUploadDTO fileUploadDTO, UserDTO user, String filePath) {
+        FileInfo fileInfo = BeanUtil.toBeanIgnoreCase(fileUploadDTO, FileInfo.class, true);
+        fileInfo.setPushDate(LocalDateTimeUtil.parseDate(DateUtil.today()));
+        fileInfo.setUserId(user.getId());
+        fileInfo.setPath(filePath);
+        fileInfo.setIsPass(UserConstants.FILE_WAIT_CHECK);
+        fileInfo.setSchoolId(user.getSchoolId());
+        fileInfo.setSchoolName(SchoolEnum.getNameByCode(user.getSchoolId()));
+        return fileInfo;
+    }
+
+    @NotNull
+    private static File saveToLocal(MultipartFile file, String fileName) {
+        File tempFile = new File(FileTypeConstants.VIDEO_FILE_PATH, fileName);
+        tempFile.getParentFile().mkdirs();
+        try (FileOutputStream fos = new FileOutputStream(tempFile);
+             InputStream is = file.getInputStream()) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                fos.write(buffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            log.error("保存文件失败");
+        }
+        return tempFile;
     }
 }
